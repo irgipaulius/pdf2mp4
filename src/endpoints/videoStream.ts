f0019242bfc46771d0a20994f1a5f76a36bc02bb
Paulius Jacinkevicius <paulius.jacinkevicius@cdn77.com>
2022-02-16 15:05:30 +0200

    increase chunk size to 5MB

--- src/endpoints/videoStream.ts
+++ src/endpoints/videoStream.ts
@@ -24,7 +24,7 @@ export function loadVideoStreamEndpoint(app: Express) {
 
       // Parse Range
       // Example: "bytes=32324-"
-      const CHUNK_SIZE = 10 ** 6; // 1MB
+      const CHUNK_SIZE = 5 * 10 ** 6; // 5MB
       const start = Number(range!.replace(/\D/g, ""));
       const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
